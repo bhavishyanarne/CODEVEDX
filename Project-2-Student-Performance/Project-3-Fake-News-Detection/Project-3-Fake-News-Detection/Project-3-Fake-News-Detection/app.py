5efce7ad2cff335e39954fb7348786702fb53148
bhavishyanarne <bhavishyanarne@gmail.com>
2026-06-15 10:32:29 +0530

    Update app.py

--- Project-2-Student-Performance/Project-3-Fake-News-Detection/Project-3-Fake-News-Detection/Project-3-Fake-News-Detection/app.py
+++ Project-2-Student-Performance/Project-3-Fake-News-Detection/Project-3-Fake-News-Detection/Project-3-Fake-News-Detection/app.py
@@ -1,1 +1,22 @@
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
 
+data = pd.read_csv("news.csv")
+
+X = data["text"]
+y = data["label"]
+
+vectorizer = CountVectorizer()
+X_vec = vectorizer.fit_transform(X)
+
+model = MultinomialNB()
+model.fit(X_vec, y)
+
+news = input("Enter News: ")
+
+test = vectorizer.transform([news])
+
+prediction = model.predict(test)
+
+print("Prediction:", prediction[0])
