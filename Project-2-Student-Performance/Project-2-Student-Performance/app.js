10d3c17699f96a43e985629c68c3e90cb0761f33
bhavishyanarne <bhavishyanarne@gmail.com>
2026-06-15 11:53:15 +0530

    Update app.js

--- Project-2-Student-Performance/Project-2-Student-Performance/app.js
+++ Project-2-Student-Performance/Project-2-Student-Performance/app.js
@@ -1,1 +1,21 @@
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
 
+data = pd.read_csv("dataset.csv")
+
+X = data[["attendance","marks","study_hours"]]
+y = data["performance"]
+
+model = LogisticRegression()
+model.fit(X,y)
+
+attendance = int(input("Attendance: "))
+marks = int(input("Marks: "))
+hours = int(input("Study Hours: "))
+
+result = model.predict([[attendance,marks,hours]])
+
+if result[0] == 1:
+    print("Good Performance")
+else:
+    print("Poor Performance")
